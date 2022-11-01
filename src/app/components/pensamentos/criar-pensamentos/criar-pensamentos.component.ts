3662d4c4b547536a20d7ff65019e37193b7e4a53
Diogo Santos <diogo.angelo@designa.com.br>
2022-11-01 21:35:34 +0000

    first commit

---
+++ src/app/components/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-pensamentos',
+  templateUrl: './criar-pensamentos.component.html',
+  styleUrls: ['./criar-pensamentos.component.scss']
+})
+export class CriarPensamentosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
